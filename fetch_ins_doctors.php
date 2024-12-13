e495bf22df6f2426b8d2430409fbec6b0d677282
Tahura-Nafees <its.tahura.nafees@gmail.com>
2024-12-13 17:58:44 +0500

    Redirection of pages-2

--- fetch_ins_doctors.php
+++ fetch_ins_doctors.php
@@ -14,26 +14,24 @@ if ($conn->connect_error) {
 // Decode incoming data
 $data = json_decode(file_get_contents("php://input"), true);
 $hospital_id = isset($data['hospital_id']) ? $data['hospital_id'] : null;
-$specialization = isset($data['specialization']) ? $data['specialization'] : null;
 
-if (!$hospital_id || !$specialization) {
+if (!$hospital_id) {
     echo json_encode([]);
     exit;
 }
 
-// Query to fetch doctors by hospital and specialization
+// Query to fetch doctors by hospital
 $query = "
-    SELECT d.name, sp.name AS specialization, h.name AS hospital, 
+    SELECT d.name, h.name AS hospital, 
            CONCAT(a.day_of_week, ' ', a.time) AS availability
     FROM Doctors d
-    JOIN Speciality sp ON d.speciality_id = sp.speciality_id
     JOIN Doctor_Hospital dh ON d.doctor_id = dh.doctor_id
     JOIN Hospitals h ON dh.hospital_id = h.hospital_id
     JOIN availability a ON d.doctor_id = a.doctor_id
-    WHERE h.hospital_id = ? AND sp.name = ?
+    WHERE h.hospital_id = ?
 ";
 $stmt = $conn->prepare($query);
-$stmt->bind_param("is", $hospital_id, $specialization);
+$stmt->bind_param("i", $hospital_id);
 $stmt->execute();
 $result = $stmt->get_result();
 
